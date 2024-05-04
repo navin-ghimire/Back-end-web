936ad8e18dc508273eeabdb81f18be0531590540
navin-ghimire <navinghimire143@gmail.com>
2024-05-04 17:50:55 +0545

    ch

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import { addOrder, getAllOrders, getOrderById } from '../controllers/oderController.js';
+import { addOrder, getAllOrders, getOrderById, getOrderByUser } from '../controllers/oderController.js';
 import { checkAdmin, checkUser } from '../middleware/checkUser.js';
 import { getAllProducts } from '../controllers/productController.js';
 
@@ -9,8 +9,7 @@ const router = express.Router();
 
 
 router.route('/').get(checkUser, checkAdmin, getAllOrders, getAllProducts).post(checkUser, addOrder)
-
-
+router.route('/user').get(checkUser, getOrderByUser);
 router.route('/:id').get(checkUser, getOrderById);
 
 
